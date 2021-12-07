65fdd2e4450cf018b8d8df6e228439158f5e2d1c
Your Name <you@example.com>
2021-12-07 16:34:43 +0800

    pubsub

--- redisex/go.mod
+++ redisex/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/bsm/redislock v0.5.0
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/go-redis/redis/v7 v7.3.0
 	github.com/gomodule/redigo/redis v0.0.0-20200429221454-e14091dffc1b
 	github.com/google/uuid v1.1.1
---
+++ redisex/pubsub.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/go-redis/redis"
+	"log"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "125.27.239.127:6379",              // 使用默认数据库
+		Password: "962012d09b8170d912f0669f6d7d9d07", // 没有密码则置空
+		DB:       0,                                  // 使用默认的数据库
+	})
+
+	pong, err := rdb.Ping().Result() // 检查是否连接
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 连接成功啦
+	log.Println(pong)
+
+	// 订阅全部消息
+	pubsub := rdb.Subscribe("__keyevent@0__:expired")
+	// 等待消息返回，原因是上一个方法不是立即返回的，囧
+	_, err = pubsub.Receive()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 用管道来接收消息
+	ch := pubsub.Channel()
+
+	// 处理消息
+	for msg := range ch {
+		log.Println(msg.Channel, ":", msg.Payload)
+	}
+
+}
+// set mykey1 3  myval1
+// set mykey2 3  myval2
